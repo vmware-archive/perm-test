fe5a34178959d72d6ab961319f6c13feb89d8adf
Jeff Pak <jeffrey.pak@emc.com>
2018-02-14 14:38:45 -0800

    Use /v2/apps to create apps
    [finishes #155203610]
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- cf/create_app.go
+++ cf/create_app.go
@@ -10,19 +10,16 @@ import (
 	"code.cloudfoundry.org/lager"
 	"github.com/cenkalti/backoff"
 	"github.com/cloudfoundry-community/go-cfclient"
+	"github.com/pivotal-cf/perm-test/cf/internal"
 )
 
-// CreateAppIfNotExists creates an app in CloudFoundry using the V3 API
+// CreateAppIfNotExists creates an app in CloudFoundry using the V2 API
 // It uses an exponential backoff strategy, returning early if it successfully creates
 // an app or the app already exists
 func CreateAppIfNotExists(logger lager.Logger, cfClient *cfclient.Client, name string, spaceGUID string) error {
-	req := &CreateAppRequestBody{
-		Name: name,
-		Relationships: SpaceRelationship{
-			Space: Space{
-				Data: Data{GUID: spaceGUID},
-			},
-		},
+	req := &CreateV2AppRequestBody{
+		Name:      name,
+		SpaceGUID: spaceGUID,
 	}
 
 	operation := func() error {
@@ -32,9 +29,19 @@ func CreateAppIfNotExists(logger lager.Logger, cfClient *cfclient.Client, name s
 			return err
 		}
 
-		r := cfClient.NewRequestWithBody("POST", "/v3/apps", b)
+		r := cfClient.NewRequestWithBody("POST", "/v2/apps", b)
 		resp, err := cfClient.DoRequest(r)
 
+		//
+		// Setting a Timeout on the HttpClient, there is a risk of
+		// the request succeeding, but being cancelled on the client side
+		// This causes the app to be created, but the backoff thinks it failed
+		// due to timeout. When it tries the operation a second time, it fails
+		// with "name must be unique in space" error, because it already exists.
+		//
+		// To get around this, we return nil if we detect this case, which causes
+		// the backoff function to stop retrying.
+		//
 		switch e := err.(type) {
 		case nil:
 			if resp.StatusCode != http.StatusCreated {
@@ -43,31 +50,27 @@ func CreateAppIfNotExists(logger lager.Logger, cfClient *cfclient.Client, name s
 			}
 			return err
 
-		case cfclient.V3CloudFoundryErrors:
+		case cfclient.CloudFoundryErrors:
 			if len(e.Errors) == 0 {
 				return err
 			}
 
-			//
-			// Setting a Timeout on the HttpClient, there is a risk of
-			// the request succeeding, but being cancelled on the client side
-			// This causes the app to be created, but the backoff thinks it failed
-			// due to timeout. When it tries the operation a second time, it fails
-			// with "name must be unique in space" error, because it already exists.
-			//
-			// To get around this, we return nil if we detect this case, which causes
-			// the backoff function to stop retrying.
-			//
 			for _, cfError := range e.Errors {
-				if cfError.Detail == "name must be unique in space" {
+				if cfError.ErrorCode == internal.AppNameTaken {
 					return nil
 				}
+			}
+		case cfclient.CloudFoundryError:
+			if e.ErrorCode == internal.AppNameTaken {
+				return nil
 			}
 
 			return err
 		default:
 			return err
 		}
+
+		return nil
 	}
 
 	err := backoff.RetryNotify(operation, backoff.NewExponentialBackOff(), func(err error, step time.Duration) {
@@ -85,9 +88,14 @@ func CreateAppIfNotExists(logger lager.Logger, cfClient *cfclient.Client, name s
 
 type SpaceGUID string
 
-type CreateAppRequestBody struct {
+type CreateV3AppRequestBody struct {
 	Name          string            `json:"name"`
 	Relationships SpaceRelationship `json:"relationships"`
+}
+
+type CreateV2AppRequestBody struct {
+	Name      string `json:"name"`
+	SpaceGUID string `json:"space_guid"`
 }
 
 type SpaceRelationship struct {
--- cf/internal/errors.go
+++ cf/internal/errors.go
@@ -3,4 +3,5 @@ package internal
 const (
 	OrganizationNameTaken = "30002"
 	SpaceNameTaken        = "40002"
+	AppNameTaken          = "100002"
 )
